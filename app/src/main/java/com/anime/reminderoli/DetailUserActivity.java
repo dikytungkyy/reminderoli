9a19f58637a80495860312af816a6a0b37cede18
dikytungkyy <44319213+dikytungkyy@users.noreply.github.com>
2019-01-06 21:52:52 +0700

    Initial commit

---
+++ app/src/main/java/com/anime/reminderoli/DetailUserActivity.java
@@ -0,0 +1,13 @@
+package com.anime.reminderoli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_user);
+    }
+}
